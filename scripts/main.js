f68e2c756f3b0ec3ba8606086aae2eec64f6efd4
Robb Fitzsimmons <robbfitzsimmons@gmail.com>
2016-11-01 18:10:19 -0500

    Update 2016-11-01T23:10:18.121Z

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/* global Vivus */
+/* eslint new-cap: 0, no-new: 0 */
+
+// Logo animation.
+new Vivus('linnaean-logo-animate', { type: 'oneByOne', duration: 300 });
+//# sourceMappingURL=main.js.map
